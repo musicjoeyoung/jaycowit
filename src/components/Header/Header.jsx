383b22d107502fa0771634bac738511524cb2b8f
Joe Young <musicjoeyoung@gmail.com>
2023-11-23 16:03:42 -0500

    initial header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,18 @@ import "./Header.scss"
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className="header">
+            <h1>Jay Cowit</h1>
+            <div className="container">
+                {/* <span>Animations are </span> */}
+                <p className="changeable">Sound Designer, Editor, Mixer</p>
+                <p className="changeable">Copy Editor, Script Writer, Consultant</p>
+                <p className="changeable">Musician</p>
+                <p className="changeable">Artist</p>
+                <p className="changeable">Director/Voice Coach</p>
+                <p className="changeable">Voice-Over Artist/On-Air Host</p>
+            </div>
+        </header>
     )
 }
 
