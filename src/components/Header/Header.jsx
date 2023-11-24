f031839d5c6b5008cd9f17e47af82d97e940bca0
Joe Young <musicjoeyoung@gmail.com>
2023-11-23 22:23:58 -0500

    header edits

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,15 @@ import "./Header.scss"
 const Header = () => {
     return (
         <header className="header">
-            <h1>Jay Cowit</h1>
-            <div className="container">
+            <h1 className="header__h1">Jay Cowit</h1>
+            <div className="titles-container">
                 {/* <span>Animations are </span> */}
-                <p className="changeable">Sound Designer, Editor, Mixer</p>
-                <p className="changeable">Copy Editor, Script Writer, Consultant</p>
-                <p className="changeable">Musician</p>
-                <p className="changeable">Artist</p>
-                <p className="changeable">Director/Voice Coach</p>
-                <p className="changeable">Voice-Over Artist/On-Air Host</p>
+                <p className="titles-container__title">Sound Designer, Editor, Mixer</p>
+                <p className="titles-container__title">Copy Editor, Script Writer, Consultant</p>
+                <p className="titles-container__title">Musician</p>
+                <p className="titles-container__title">Artist</p>
+                <p className="titles-container__title">Director/Voice Coach</p>
+                <p className="titles-container__title">Voice-Over Artist/On-Air Host</p>
             </div>
         </header>
     )
