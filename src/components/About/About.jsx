1658e719955358c8f814f67f50bdcbc308f450b4
Joe Young <musicjoeyoung@gmail.com>
2023-11-24 21:54:38 -0500

    styles corrections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,17 @@
 import "./About.scss"
+import jay from "../../assets/images/JayCowit.png"
 
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <h2>About</h2>
+            <div className="about-container">
+                <p className="about-container__bio">
+                    Hi! I’m Jay Cowit. For over 20 years I’ve worked in radio, audio, music and other media. I like making cool things and helping other folks make cool things. I’m an expert sound designer, audio mixer, editor, voice coach, script writer and copy editor. I also make music and produce/compose scores for radio, podcasts and video games. I’m available for consultant work, freelance opportunities and coaching. Let’s talk about the cool thing we could make together. Thanks for hanging out!
+                </p>
+                <img src={jay} alt="Jay Cowit" className="about-container__img" />
+            </div>
+        </>
     )
 }
 
