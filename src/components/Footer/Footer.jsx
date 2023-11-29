302b687a14cd124a0c2dc4686229056ea5128f25
Joe Young <musicjoeyoung@gmail.com>
2023-11-28 22:37:46 -0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
 import "./Footer.scss"
-import bandcamp from "../../assets/images/bandcamp-logo.svg"
+import bandcamp from "../../assets/images/bandcamp.svg"
+import email from "../../assets/images/email.svg"
+import facebook from "../../assets/images/facebook.svg"
+import instagram from "../../assets/images/instagram.svg"
+import linkedin from "../../assets/images/linkedin.svg"
+import twitter from "../../assets/images/twitter.svg"
 
 const Footer = () => {
     return (
-        <div>Footer
-            <img src={bandcamp} />
-        </div>
+        <footer className="footer">
+            <div className="image-container">
+                <img className="image-container__img" src={bandcamp} />
+                <img className="image-container__img" src={email} />
+                <img className="image-container__img" src={facebook} />
+                <img className="image-container__img" src={instagram} />
+                <img className="image-container__img" src={linkedin} />
+                <img className="image-container__img" src={twitter} />
+            </div>
+            <h6 className="footer__h6">© Joseph Young. All rights reserved.</h6>
+            <h6 className="footer__h6"><a href="mailto:joseph.m.young2@gmail.com">Contact the Webmaster</a></h6>
+        </footer>
     )
 }
 
