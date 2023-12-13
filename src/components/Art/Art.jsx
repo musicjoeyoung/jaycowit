922d33822aa2fcf93707f4b5714cfc53e7a4fa95
Joe Young <musicjoeyoung@gmail.com>
2023-12-12 20:41:43 -0500

    Art updates; see more/less; array of urls

--- src/components/Art/Art.jsx
+++ src/components/Art/Art.jsx
@@ -4,6 +4,11 @@ import './Art.scss';
 const Art = () => {
     const [modalVisible, setModalVisible] = useState(false);
     const [selectedImage, setSelectedImage] = useState('');
+    const [isVisible1, setIsVisible1] = useState(false);
+
+    const toggle1 = () => {
+        setIsVisible1(!isVisible1);
+    };
 
     const openModal = (image) => {
         setSelectedImage(image);
@@ -15,28 +20,44 @@ const Art = () => {
         setModalVisible(false);
     };
 
+    const images = [
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415387/Jay%20Cowit/QKTP0189_wauzzp.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415383/Jay%20Cowit/PWJK9821_qyb42z.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415383/Jay%20Cowit/ACWZ9151_wclza1.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415383/Jay%20Cowit/GRPQ9170_xwb3kq.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415382/Jay%20Cowit/PTKG5709_wriy54.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415381/Jay%20Cowit/HXBE2983_sueobm.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415374/Jay%20Cowit/IMG_5590_cawvss.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415372/Jay%20Cowit/BWSF6115_wiftvd.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415369/Jay%20Cowit/IMG_9196_umbxah.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415364/Jay%20Cowit/IMG_9521_vzblwl.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415359/Jay%20Cowit/IMG_9742_jg6n8c.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415388/Jay%20Cowit/SXOT9072_ccgpcp.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415382/Jay%20Cowit/IMG_7777_zfwwgu.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415380/Jay%20Cowit/IMG_8041_gc1dgg.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415370/Jay%20Cowit/EMNQ2604_bbhfkc.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415368/Jay%20Cowit/UDOK5521_ua2p3v.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415341/Jay%20Cowit/IMG_0578_jnk9qo.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415339/Jay%20Cowit/IMG_0441_scwdwg.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415330/Jay%20Cowit/IMG_1476_1_fvxoe0.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415322/Jay%20Cowit/IMG_3486_brqwpb.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415320/Jay%20Cowit/IMG_2404_fd0t5o.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415315/Jay%20Cowit/IMG_2401_h6f38z.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415315/Jay%20Cowit/IMG_0156_gtjbtz.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415309/Jay%20Cowit/IMG_2964_g4ujoa.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415307/Jay%20Cowit/UKTF7495_egkisb.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415307/Jay%20Cowit/UROP6979_b2ghlg.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415307/Jay%20Cowit/IMG_2978_s9rosj.jpg',
+        'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415305/Jay%20Cowit/IHCU7607_tuadg8.jpg'
+    ];
+
+    const visibleImages = isVisible1 ? images : images.slice(0, 8);
+
     return (
         <section>
             <h2 id="art">Art</h2>
             <div className="art-container">
-                {[
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415387/Jay%20Cowit/QKTP0189_wauzzp.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415383/Jay%20Cowit/PWJK9821_qyb42z.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415383/Jay%20Cowit/ACWZ9151_wclza1.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415383/Jay%20Cowit/GRPQ9170_xwb3kq.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415382/Jay%20Cowit/PTKG5709_wriy54.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415381/Jay%20Cowit/HXBE2983_sueobm.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415374/Jay%20Cowit/IMG_5590_cawvss.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415372/Jay%20Cowit/BWSF6115_wiftvd.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415369/Jay%20Cowit/IMG_9196_umbxah.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415364/Jay%20Cowit/IMG_9521_vzblwl.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415359/Jay%20Cowit/IMG_9742_jg6n8c.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415388/Jay%20Cowit/SXOT9072_ccgpcp.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415382/Jay%20Cowit/IMG_7777_zfwwgu.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415380/Jay%20Cowit/IMG_8041_gc1dgg.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415370/Jay%20Cowit/EMNQ2604_bbhfkc.jpg',
-                    'https://res.cloudinary.com/dfbuwtl5q/image/upload/v1702415368/Jay%20Cowit/UDOK5521_ua2p3v.jpg',
-                ].map((imageUrl, index) => (
+                {visibleImages.map((imageUrl, index) => (
                     <img
                         key={index}
                         className="art-container__art"
@@ -46,6 +67,12 @@ const Art = () => {
                     />
                 ))}
             </div>
+
+            {images.length > 5 && (
+                <p className="art-container__p" onClick={toggle1}>
+                    {isVisible1 ? 'Show Less' : 'Show More'}
+                </p>
+            )}
 
             {modalVisible && (
                 <div className="modal-overlay" onClick={closeModal}>
