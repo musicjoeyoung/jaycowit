04ea6d403043f9d60d4630dc5338eb79d2239913
Joe Young <musicjoeyoung@gmail.com>
2023-12-30 07:14:48 -0500

    removed max-width from apple podcast embed

--- src/components/SoundDesign/SoundDesign.jsx
+++ src/components/SoundDesign/SoundDesign.jsx
@@ -173,7 +173,7 @@ const SoundDesign = ({ darkMode }) => {
                         allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
                         frameBorder="0"
                         height="450"
-                        style={{ width: '100%', maxWidth: '660px', overflow: 'hidden', borderRadius: '10px' }}
+                        style={{ width: '100%', overflow: 'hidden', borderRadius: '10px' }}
                         sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
                         src="https://embed.podcasts.apple.com/us/podcast/how-to-vote-in-america/id1499899716"
                     ></iframe>
